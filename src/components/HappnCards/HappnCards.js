5f3b6cd9c645a45e7ca4a2b32d5bc2180b4758b4
FranciscoHeronildo <heronildo.2010@hotmail.com>
2022-04-25 01:47:32 -0300

    Terminando merge
    Co-authored-by: Natilorens <nathalialorenacardoso@gmail.com>

--- src/components/HappnCards/HappnCards.js
+++ src/components/HappnCards/HappnCards.js
@@ -4,6 +4,7 @@ import "./HappnCards.css";
 import { getPeople } from "../../api/api";
 import Graph from "../../algorithm/Graph.js";
 import { mergeSort } from "../../algorithm/merge.js";
+import SwipeButtons from "../SwipeButtons/SwipeButtons";
 
 function HappnCards() {
   const [people, setPeople] = useState([]);
@@ -11,96 +12,96 @@ function HappnCards() {
 
   async function fetchPeople() {
     const peopleList = await getPeople();
-    setPeople(peopleList)
-    createGraph(peopleList)
-    sorting(peopleList)
+    setPeople(peopleList);
+    createGraph(peopleList);
+    sorting(peopleList);
   }
 
-
-
   async function createGraph(peopleList) {
-
     let g = new Graph();
 
-    let first = ""
+    let first = "";
     let distances = await peopleList.map((person, index) => {
-      g.addVertex(person.id)
+      g.addVertex(person.id);
       if (index === 0) {
-        first = person.id
+        first = person.id;
       }
       if (index > 0) {
-        console.log(`id = ${first}`, person.id, person.distance)
+        console.log(`id = ${first}`, person.id, person.distance);
         g.addEdge(`${first}`, person.id, person.distance);
       }
-      return { id: person.id, distance: person.distance }
-    })
+      return { id: person.id, distance: person.distance };
+    });
 
-    setDistance(distances)
-    console.log(distances)
+    setDistance(distances);
+    console.log(distances);
 
     g.dijkstra(distances[0].id);
   }
 
-
-
-
   async function sorting(peopleList) {
-
     let distances = await peopleList.map((person) => {
-      console.log('Os valores da distancia: ', person.distance);
-      return (person.distance)
-    })
+      console.log("Os valores da distancia: ", person.distance);
+      return person.distance;
+    });
 
-    setDistance(distances)
-    console.log("aqui", distances)
+    setDistance(distances);
+    console.log("aqui", distances);
 
-    let ids = mergeSort(distances)
-    console.log("id", ids)
+    let ordDistances = mergeSort(distances);
+    console.log("id", ordDistances);
 
-    let ordenado = await peopleList.forEach((person, index) => {
+    let ordPeopleList = [];
 
-    })
-    console.log("ord", ordenado)
+    for (let j = 0; j < ordDistances.length; j++) {
+      for (let i = 0; i < peopleList.length; i++) {
+        if (ordDistances[j] === peopleList[i].distance) {
+          console.log("for", peopleList[i].distance);
+          ordPeopleList.push(peopleList[i]);
+        }
+      }
+    }
+
+    ordPeopleList.reverse();
+
+    setPeople(ordPeopleList);
+
+    console.log("Lista de Pessoas Ordenadas", ordPeopleList);
   }
 
-
-
-
   useEffect(() => {
-    fetchPeople()
-  }, [])
+    fetchPeople();
+  }, []);
 
-  const swiped = (nameToDelete) => {
-    console.log("removing: " + nameToDelete);
-
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing: ", direction, nameToDelete);
   };
 
   return (
-    <div className="happnCards">
-      <div className="happnCardsContainer">
-        {people && people.map((person) => (
-          <HappnCard
-            className="swipe"
-            key={person.name}
-            preventSwipe={["up", "down"]}
-            onSwipe={(dir) => swiped(dir, person.distance)} //swiped(dir, person.name)}
-            onCardLeftScreen={() => { }} //outOfFrame(person.name)}
-          >
-
-            <div
-              className="card"
-              style={{ backgroundImage: `url(${person.avatar})` }}
-            >
-              <h2>{person.name}</h2>
-              <h3>{person.distance} KM</h3>
-            </div>
-
-            <></>
-
-          </HappnCard>
-        ))}
+    <>
+      <div className="happnCards">
+        <div className="happnCardsContainer">
+          {people &&
+            people.map((person) => (
+              <HappnCard
+                className="swipe"
+                key={person.id}
+                preventSwipe={["up", "down"]}
+                onSwipe={(dir) => swiped(dir, person.distance)} //swiped(dir, person.name)}
+                onCardLeftScreen={() => {}} //outOfFrame(person.name)}
+              >
+                <div
+                  className="card"
+                  style={{ backgroundImage: `url(${person.avatar})` }}
+                >
+                  <h2>{person.name}</h2>
+                  <h3>{person.distance} KM</h3>
+                </div>
+              </HappnCard>
+            ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
